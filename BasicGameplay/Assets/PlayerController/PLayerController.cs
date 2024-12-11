b7b2d500cb283eaf2e2b4b5a5f77085853a2e71b
natycg <checon@roverkids.org>
2024-12-11 14:13:57 -0500

    ide

--- BasicGameplay/Assets/PlayerController/PLayerController.cs
+++ BasicGameplay/Assets/PlayerController/PLayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PLayerController : MonoBehaviour
 {
     public float horizantalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,11 @@ public class PLayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < - 10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
          horizantalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizantalInput * Time.deltaTime * speed);
     }
 } 
