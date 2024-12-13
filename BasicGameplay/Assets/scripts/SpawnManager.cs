11c1b8d36a2d9520f69a1bcd82703206312e73ab
natycg <checon@roverkids.org>
2024-12-13 14:00:46 -0500

    spawn interval

--- BasicGameplay/Assets/scripts/SpawnManager.cs
+++ BasicGameplay/Assets/scripts/SpawnManager.cs
@@ -7,12 +7,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -20,10 +21,15 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-           
-            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20, 20) , 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20, 20), 0, 20), animalPrefabs[animalIndex].transform.rotation);
     }
 }
